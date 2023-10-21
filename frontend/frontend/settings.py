9a985c9b4c1d37ada3b9062f8fe84b885189aa62
Edward Zhou <eddyspaghette@gmail.com>
2023-10-21 14:52:03 -0700

    Setup Postgres and remove useless files

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for frontend project.
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-p8ic1rurb1g)uyzy_d#6m^oo+013^m+ez&19@4j#fznk8qk)u&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -72,11 +73,15 @@ WSGI_APPLICATION = 'frontend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_DB'),
+        'PORT': 5432,
     }
 }
 
