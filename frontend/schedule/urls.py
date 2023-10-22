5c97a1d71e0f3656324985a36b916b4cb32d2491
XZTDean <xztdean@gmail.com>
2023-10-21 17:35:53 -0700

    Add api for creating demo data

--- frontend/schedule/urls.py
+++ frontend/schedule/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("getevents/", views.getEvents, name="events"),
+    path("getevents/", views.get_events, name="events"),
+    path("createdemodata/", views.create_demo_data, name="create_demo_data"),
 ]
--- frontend/schedule/views.py
+++ frontend/schedule/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db.models import F
 from django.http import JsonResponse
 
@@ -10,6 +12,12 @@ def index(request):
     return render(request, "schedule/index.html")
 
 
-def getEvents(request):
+def get_events(request):
     events = Event.objects.annotate(title=F("name"), start=F("start_time"), end=F("end_time")).values("title", "start", "end")
     return JsonResponse(list(events), safe=False)
+
+
+def create_demo_data(request):
+    Event.objects.create(name="Event 1", start_time=datetime(2023, 10, 21, 19, 0, 0), end_time=datetime(2023, 10, 21, 20, 0, 0))
+    Event.objects.create(name="Event 2", start_time=datetime(2023, 10, 22, 8, 0, 0), end_time=datetime(2023, 10, 22, 10, 0, 0))
+    return JsonResponse({"success": True})
