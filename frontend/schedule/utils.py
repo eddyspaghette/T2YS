e256270b338cd733871fed968915591ea6947b16
akn <anhkhoan@sfu.ca>
2023-10-21 20:23:44 -0700

    add.

--- frontend/schedule/utils.py
+++ frontend/schedule/utils.py
@@ -4,4 +4,8 @@ from .predict_model.main import predict
 
 def get_new_event(prompt):
     output = predict(prompt)
+
+    if (output["name"] is None or output["start_time"] is None or output["end_time"] is None):
+        return
+        
     Event.objects.create(name=output["name"], start_time=output["start_time"], end_time=output["end_time"])
