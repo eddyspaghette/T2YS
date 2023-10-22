5c468dd04ab5db176f539155c44564ff7eded09a
XZTDean <xztdean@gmail.com>
2023-10-21 17:19:24 -0700

    Add api to get events

--- frontend/schedule/views.py
+++ frontend/schedule/views.py
@@ -1,4 +1,15 @@
+from django.db.models import F
+from django.http import JsonResponse
+
 from django.shortcuts import render
+
+from .models import Event
+
 
 def index(request):
     return render(request, "schedule/index.html")
+
+
+def getEvents(request):
+    events = Event.objects.annotate(title=F("name"), start=F("start_time"), end=F("end_time")).values("title", "start", "end")
+    return JsonResponse(list(events), safe=False)
