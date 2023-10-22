c59e3eb99a88830ff9ffbc404c951e6ca75f057c
XZTDean <xztdean@gmail.com>
2023-10-21 20:39:28 -0700

    Parse the time

--- frontend/schedule/utils.py
+++ frontend/schedule/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from .models import Event
 from .predict_model.main import predict
 
@@ -7,5 +9,8 @@ def get_new_event(prompt):
 
     if (output["name"] is None or output["start_time"] is None or output["end_time"] is None):
         return
+
+    start_time = datetime.strptime("%Y-%m-%dT%H:%M:%S", output["start_time"])
+    end_time = datetime.strptime("%Y-%m-%dT%H:%M:%S", output["end_time"])
         
-    Event.objects.create(name=output["name"], start_time=output["start_time"], end_time=output["end_time"])
+    Event.objects.create(name=output["name"], start_time=start_time, end_time=end_time)
